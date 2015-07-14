4ddeeb3323295d3bb6d9727fb7fbed82ff5db215
Dan Moore <dan@vaporwa.re>
2015-07-14 17:23:46 -0400

    users have names, slugs, and profile pages, take 1

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_permitted_params, only: [:create, :update]
+  
   skip_authorization_check
+
+  private
+  def configure_permitted_params
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:name, :email, :password, :password_confirmation)
+    end
+  end
 end
