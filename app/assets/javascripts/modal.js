7909fb976ff766ce3a2c1ac1ebb0769a919a7c3e
rjcox <robertcox11@gmail.com>
2015-07-30 16:48:39 -0400

    attempt at making modal pretty

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -8,36 +8,58 @@ $(document).ready(function(){
   $("#company-form").hide();
   $("#collection-form").hide();
 
-  // Person form show, others hide
   $("#person-btn").on("click", function(event){
     event.preventDefault();
+    // Make button large
+    $(".form-select-btn.btn-lg").removeClass("btn-lg");
+    $(this).addClass("btn-lg", 1000);
+    // Person form show, others hide
     $("#person-form").show();
     $("#company-form, #collection-form").hide();
     // Change modal body class to person-body
     $("#new-modal").addClass("person-body");
     $("#new-modal").removeClass("company-body");
     $("#new-modal").removeClass("collection-body");
+    // Change input class to person-form-input
+    $(".form-input").addClass("person-form-input");
+    $(".form-input").removeClass("company-form-input");
+    $(".form-input").removeClass("collection-form-input");
   });
 
-  // Company form show, others hide
   $("#company-btn").on("click", function(event){
     event.preventDefault();
+    // Make button large
+    $(".form-select-btn.btn-lg").removeClass("btn-lg");
+    $(this).addClass("btn-lg", 10000);
+    // Company form show, others hide
     $("#company-form").show();
     $("#person-form, #collection-form").hide();
     // Change modal body class to company-body
     $("#new-modal").addClass("company-body");
     $("#new-modal").removeClass("person-body");
     $("#new-modal").removeClass("collection-body");
+    // Change input class to person-form-input
+    $(".form-input").addClass("company-form-input");
+    $(".form-input").removeClass("person-form-input");
+    $(".form-input").removeClass("collection-form-input");
   });
-  // Collection form show, others hide
+  
   $("#collection-btn").on("click", function(event){
     event.preventDefault();
+    // Make button large
+    $(".form-select-btn.btn-lg").removeClass("btn-lg");
+    $(this).addClass("btn-lg", 1000);
+    // Collection form show, others hide
     $("#collection-form").show();
     $("#person-form, #company-form").hide();
     // Change modal body class to collection-body
     $("#new-modal").addClass("collection-body");
     $("#new-modal").removeClass("company-body");
     $("#new-modal").removeClass("person-body");
+    // Change input class to person-form-input
+    $(".form-input").addClass("collection-form-input");
+    $(".form-input").removeClass("company-form-input");
+    $(".form-input").removeClass("person-form-input");
   });
 
   // Change background color to match buttons
