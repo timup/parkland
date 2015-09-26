b6b50c69958fa1a9214f079fbb709c2ce4c7bac8
Jeff Schroeder <jbschrades@gmail.com>
2015-09-26 18:01:22 +0000

    add devise invitable to admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917161013) do
+ActiveRecord::Schema.define(version: 20150926180012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,20 @@ ActiveRecord::Schema.define(version: 20150917161013) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer  "invitation_limit"
+    t.integer  "invited_by_id"
+    t.string   "invited_by_type"
+    t.integer  "invitations_count",      default: 0
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
+  add_index "admins", ["invitation_token"], name: "index_admins_on_invitation_token", unique: true, using: :btree
+  add_index "admins", ["invitations_count"], name: "index_admins_on_invitations_count", using: :btree
+  add_index "admins", ["invited_by_id"], name: "index_admins_on_invited_by_id", using: :btree
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "collections", force: :cascade do |t|
@@ -89,16 +100,6 @@ ActiveRecord::Schema.define(version: 20150917161013) do
   add_index "resource_collections", ["collection_id"], name: "index_resource_collections_on_collection_id", using: :btree
   add_index "resource_collections", ["resource_id"], name: "index_resource_collections_on_resource_id", using: :btree
 
-  create_table "resource_tags", force: :cascade do |t|
-    t.integer  "resource_id"
-    t.integer  "tag_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "resource_tags", ["resource_id"], name: "index_resource_tags_on_resource_id", using: :btree
-  add_index "resource_tags", ["tag_id"], name: "index_resource_tags_on_tag_id", using: :btree
-
   create_table "resources", force: :cascade do |t|
     t.string   "name"
     t.string   "href"
@@ -117,13 +118,6 @@ ActiveRecord::Schema.define(version: 20150917161013) do
 
   add_index "resources", ["slug"], name: "index_resources_on_slug", unique: true, using: :btree
   add_index "resources", ["user_id"], name: "index_resources_on_user_id", using: :btree
-
-  create_table "tags", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -151,7 +145,5 @@ ActiveRecord::Schema.define(version: 20150917161013) do
   add_foreign_key "collections", "users"
   add_foreign_key "resource_collections", "collections"
   add_foreign_key "resource_collections", "resources"
-  add_foreign_key "resource_tags", "resources"
-  add_foreign_key "resource_tags", "tags"
   add_foreign_key "resources", "users"
 end
