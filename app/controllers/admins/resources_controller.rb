73190e50ecd01633d1f7f701a5a60efe3331519d
Jeff Schroeder <jbschrades@gmail.com>
2015-11-10 19:56:51 +0000

    admins can edit resources

--- app/controllers/admins/resources_controller.rb
+++ app/controllers/admins/resources_controller.rb
@@ -1,13 +1,29 @@
 class Admins::ResourcesController < AdminsController
-  before_action :set_resource, only: :show
+  before_action :set_resource, only: [:show, :edit, :update]
 
   authorize_resource
 
+  # GET admins/resources
   def index
     @resources = Resource.alphabetical
   end
+
+  # GET admins/resources/1/edit
+  def edit
+  end
   
+  # GET admins/resources/1/
   def show
+  end
+
+  # PATCH/PUT admins/resources/1
+  def update
+    @resource = @resource.becomes(Resource)
+    if @resource.update(resource_params)
+      redirect_to admin_resource_path(@resource), notice: 'Resource was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   private
@@ -15,6 +31,10 @@ class Admins::ResourcesController < AdminsController
     # Use callbacks to share common setup or constraints between actions.
     def set_resource
       @resource = Resource.find(params[:id])
+    end
+
+    def resource_params
+      params.require(:resource).permit(:id, :description, :href, :name, :image, :type)
     end
 
 end
